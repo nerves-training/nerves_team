3b8de7e810d8827e8f40f92ca1af44c0d5eabce3
Justin Schneck <jschneck@mac.com>
2019-01-30 16:26:08 -0500

    Step 9 - Add PhoenixClient

--- nerves_team_ui/lib/nerves_team_ui.ex
+++ nerves_team_ui/lib/nerves_team_ui.ex
@@ -7,9 +7,12 @@ defmodule NervesTeamUI do
     # load the viewport configuration from config
     main_viewport_config = Application.get_env(:nerves_team_ui, :viewport)
 
+    socket_opts = Application.get_env(:nerves_team_ui, :socket)
+
     # start the application with the viewport
     children = [
-      {Scenic, viewports: [main_viewport_config]}
+      {Scenic, viewports: [main_viewport_config]},
+      {PhoenixClient.Socket, {socket_opts, name: NervesTeamUI.Socket}}
     ]
 
     Supervisor.start_link(children, strategy: :one_for_one)
