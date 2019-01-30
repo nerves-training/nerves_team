e070346b1305f79de8700532fd9a705f7af2a6c6
Justin Schneck <jschneck@mac.com>
2019-01-30 15:34:16 -0500

    Step 2 - Adding NervesHub
    * Add the NervesHub package dependencies

--- nerves_team_device/mix.exs
+++ nerves_team_device/mix.exs
@@ -47,6 +47,8 @@ defmodule NervesTeamDevice.MixProject do
       # Dependencies for all targets except :host
       {:nerves_runtime, "~> 0.6", targets: @all_targets},
       {:nerves_init_gadget, "~> 0.4", targets: @all_targets},
+      {:nerves_hub, "~> 0.2", targets: @all_targets},
+      {:nerves_key, "~> 0.3", targets: @all_targets},
 
       # Dependencies for specific targets
       {:nerves_system_rpi0, "~> 1.8", runtime: false, targets: :rpi0},
