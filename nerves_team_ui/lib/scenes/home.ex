07c5f5c7f75751036de73d937a431137e8f0a641
Justin Schneck <jschneck@mac.com>
2019-01-30 16:18:01 -0500

    Step 7 - Update home scene
    * Build scene graph at runtime
    * Center text

--- nerves_team_ui/lib/scenes/home.ex
+++ nerves_team_ui/lib/scenes/home.ex
@@ -9,11 +9,7 @@ defmodule NervesTeamUI.Scene.Home do
   # import Scenic.Components
 
   @note """
-    This is a very simple starter application.
-
-    If you want a more full-on example, please start from:
-
-    mix scenic.new.example
+  NervesTeam
   """
 
   @text_size 24
@@ -27,20 +23,18 @@ defmodule NervesTeamUI.Scene.Home do
     # a transparent full-screen rectangle to catch user input
     {:ok, %ViewPort.Status{size: {width, height}}} = ViewPort.info(opts[:viewport])
 
-    # show the version of scenic and the glfw driver
-    scenic_ver = Application.spec(:scenic, :vsn) |> to_string()
-    glfw_ver = Application.spec(:scenic, :vsn) |> to_string()
+    center = {0.5 * width, 0.5 * height}
 
     graph =
       Graph.build(font: :roboto, font_size: @text_size)
       |> add_specs_to_graph([
-        text_spec("scenic: v" <> scenic_ver, translate: {20, 40}),
-        text_spec("glfw: v" <> glfw_ver, translate: {20, 40 + @text_size}),
-        text_spec(@note, translate: {20, 120}),
-        rect_spec({width, height})
+        text_spec(@note, text_align: :center, translate: center),
       ])
 
-    {:ok, graph, push: graph}
+    {:ok, %{
+      graph: graph,
+      viewport: opts[:viewport]
+    }, push: graph}
   end
 
   def handle_input(event, _context, state) do
